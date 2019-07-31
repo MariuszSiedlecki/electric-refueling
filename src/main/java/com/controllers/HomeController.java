ffb58dcdca252d6ecf9052aa13b0226750459c1c
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-07-31 13:17:23 +0200

    thymeleaf

--- src/main/java/com/controllers/HomeController.java
+++ src/main/java/com/controllers/HomeController.java
@@ -1,13 +1,22 @@
 package com.controllers;
 
+import com.services.PlaceService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @GetMapping({"/", "/api", "/api/v1"})
-    public String message() {
+    private PlaceService placeService;
+
+    public HomeController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @GetMapping("/")
+    public String homePage(Model model){
+        model.addAttribute("places", placeService.getPlacesDto());
         return "index";
     }
 }
