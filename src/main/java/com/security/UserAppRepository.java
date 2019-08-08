abc8bdad41ecf7b3061e6ca228054cb9b1356845
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-08-08 15:38:21 +0200

    userAppRepository

---
+++ src/main/java/com/security/UserAppRepository.java
@@ -0,0 +1,14 @@
+package com.security;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserAppRepository extends JpaRepository<UserApp,Integer> {
+    @Query(value = "SELECT u FROM UserApp u where u.userName=?1")
+    Optional<UserApp> findUserAppByName(String username);
+}
