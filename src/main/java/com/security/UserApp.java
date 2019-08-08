be21bfe26683b681f79f501e7ac3fe72a36cc24b
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-08-08 15:37:32 +0200

    Spring security

---
+++ src/main/java/com/security/UserApp.java
@@ -0,0 +1,38 @@
+package com.security;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name="user", schema = "public")
+public class UserApp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String userName;
+    private String password;
+    private int active;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+    @JoinTable(name = "user_role",
+            joinColumns =
+            @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public UserApp(UserApp userApp) {
+        this.userName = userApp.getUserName();
+        this.password = userApp.getPassword();
+        this.active = userApp.getActive();
+        this.roles = userApp.getRoles();
+    }
+}
