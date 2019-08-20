8ef0c27d70ca0370c66994a1117babb651df75b7
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-08-20 23:56:05 +0200

    create file xls

--- src/main/java/com/controllers/PlaceController.java
+++ src/main/java/com/controllers/PlaceController.java
@@ -1,23 +1,37 @@
 package com.controllers;
 
+import com.commons.Naming;
+import com.commons.extras.CreatorXls;
+import com.commons.extras.DirectoryCreator;
 import com.models.Place;
 import com.models.PlaceDto;
 import com.services.PlaceService;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class PlaceController {
 
+
     private PlaceService placeService;
 
     public PlaceController(PlaceService placeServices) {
         this.placeService = placeServices;
+        DirectoryCreator.createDirectory();
     }
 
     @GetMapping("/api/v1/place")
@@ -33,6 +47,10 @@ public class PlaceController {
     @GetMapping(value = "/api/v1/places",produces = "application/json")
     public List<Place> getPlaces() {
         return placeService.getPlaces();
+    }
+    @GetMapping(value = "/api/v1/places/file")
+    public void getPlacesInFile(@RequestParam String filename) throws IOException, NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        placeService.getFile(filename);
     }
 
     @GetMapping(value = "/api/v1/places/dto", produces = "application/json")
@@ -71,5 +89,14 @@ public class PlaceController {
             return new ResponseEntity<>(HttpStatus.OK);
         }
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @GetMapping("/api/v1/places/download/file/xls/{filename}")
+    public ResponseEntity<Resource> downloadXls(@PathVariable String filename) throws IOException {
+        Resource resource = new UrlResource(Paths.get(Naming.LOCAL_PATH + filename).toUri());
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType("application/excel"))
+                .header(HttpHeaders.CONTENT_DISPOSITION,"attachment; filename=\"" + resource.getFile().getName() +"\"")
+                .contentLength(resource.getFile().length())
+                .body(resource);
     }
 }
