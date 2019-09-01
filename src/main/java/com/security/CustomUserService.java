4b59a0b2745eb412723acd95e3d0e7eb6d2cf3a1
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-09-01 23:52:55 +0200

    register new user

--- src/main/java/com/security/CustomUserService.java
+++ src/main/java/com/security/CustomUserService.java
@@ -4,15 +4,20 @@ package com.security;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
 
 @Service
 public class CustomUserService implements UserDetailsService {
 
     private UserAppRepository userAppRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public CustomUserService(UserAppRepository userAppRepository) {
+    public CustomUserService(UserAppRepository userAppRepository, PasswordEncoder passwordEncoder) {
         this.userAppRepository = userAppRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -20,7 +25,19 @@ public class CustomUserService implements UserDetailsService {
         return userAppRepository
                 .findUserAppByName(username)
                 .map(CustomUserDetails::new)
-                .orElseThrow(()-> new UsernameNotFoundException("User not find"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not find"));
+    }
+
+    public void saveUserApp(LoginUser loginUser) {
+
+        UserApp result = UserApp
+                .builder()
+                .userName(loginUser.getUsername())
+                .password(passwordEncoder.encode(loginUser.getPassword()))
+                .active(1)
+                .roles(new HashSet<>())
+                .build();
+        userAppRepository.save(result);
     }
 }
 
